0d3bc84e70835f24c6f25cf6678c4a5a9d572bf2
JDBgit9 <info@jamesdbeeks.com>
2020-10-08 23:30:29 -0400

    git it to connect

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 var bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
+var path = require("path")
+var PORT = process.env.PORT|| 8080;
  
-app.get('/', function (req, res) {
-  res.send('Notes')
-})
- 
-app.listen(3000)
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'db')));
+app.use(express.json());
 
+require("./Routes/apiroutes")
+require("./Routes/htmlroutes")
 
 // parse various different custom JSON types as JSON
 app.use(bodyParser.json({ type: 'application/*+json' }))
@@ -17,3 +20,7 @@ app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
  
 // parse an HTML body into a string
 app.use(bodyParser.text({ type: 'text/html' }))
+
+app.listen(PORT, function() {  console.log("App listening on PORT: http://localhost:" + PORT);
+
+});
