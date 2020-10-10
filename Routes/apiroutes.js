3333a98ec46bd91545f046a1a703845431bbb84f
JDBgit9 <info@jamesdbeeks.com>
2020-10-09 23:15:43 -0400

    its finally working

--- Routes/apiroutes.js
+++ Routes/apiroutes.js
@@ -30,25 +30,24 @@ module.exports = function (app) {
         });
     });
     //selects the notes file and created a function
-    app.post("/api/notes"), function (req, res) {
+    app.post("/api/notes", function (req, res) {
         console.log(req.body);
         //entered array for scope purposes
-        // notesArray = [];
-        // notesArray.push(req.body);
+        notesArray = [];
+        notesArray.push(req.body);
         //reads the outputPath value (db.json) in english
         fs.readFile(outputPath, 'utf8', (err, data) => {
             if (err) throw err;
            
             data = JSON.parse(data);
             for (let i = 0; i < data.length; i++) {
-              
                 notesArray.push(data[i])
             }
-            for (let i = 0; i < notesArray.length; index++) {
+            for (let i = 0; i < notesArray.length; i++) {
                 notesArray[i].id = i + 1;
             }
             res.send(notesArray);
-            console.log(notesArray);
+            // console.log(notesArray);
             //writes the notes array values to the db.json file and logs success if no error occurs
             fs.writeFile(outputPath, JSON.stringify(notesArray), function (err) {
                 if (err) {
@@ -58,7 +57,7 @@ module.exports = function (app) {
                 }
             })
         });
-    }
+    })
 
 
 app.delete("/api/notes/:id", (req, res) => {
