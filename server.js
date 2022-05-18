5d3442c1ff517c315f77c25b634f1d9ba0961638
JDBgit9 <jamesthejust99@outlook.com>
2022-05-18 14:48:42 -0400

    deleted storejs

--- server.js
+++ server.js
@@ -1,29 +1,55 @@
 // var bodyParser = require('body-parser')
 const express = require('express')
-const app = express()
 const fs = require("fs");
-const path = require("path")
+const notes = require("./db/db.json");
+const path = require("path");
+const uuid = require("uuid");
+const { DH_CHECK_P_NOT_SAFE_PRIME } = require("constants");
+
+const app = express()
 const PORT = process.env.PORT|| 8080;
  
+// Middleware
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'db')));
 app.use(express.json());
+app.use(express.static("public"));
 
-require("./Routes/apiroutes")(app);
-require("./Routes/htmlroutes")(app);
-
-// parse various different custom JSON types as JSON
-// app.use(bodyParser.json({ type: 'application/*+json' }))
- 
-// parse some custom thing into a Buffer
-// app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
- 
-// parse an HTML body into a string
-// app.use(bodyParser.text({ type: 'text/html' }))
-app.get("/", function(req, res) {
-res.sendFile(path.join(__dirname,"/public/index.html"));
+//Setting routes for APIs
+//This gets notes saved and joins it in db.json
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"))
 });
+
+// Post function to add new notes to db.json
+app.post("/api/notes", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const newNotes = req.body;
+    newNotes.id = uuid.v4();
+    notes.push(newNotes);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+    res.json(notes);
+});
+
+//used for deleting notes
+app.delete("/api/notes/:id", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const delNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
+    fs.writeFileSync("./db/db.json", JSON.stringify(delNote));
+    res.json(delNote);
+})
+
+
+//HTML calls
+//calls home page
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+//call for notes.html
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+
 
 app.listen(PORT, function() {  console.log("App listening on PORT: http://localhost:" + PORT);
 
